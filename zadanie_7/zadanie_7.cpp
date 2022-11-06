71e15b744dbcf42cf6fccf17a6e621c94cd087ad
Ninel39 <ninel.leynveber@mail.ru>
2022-11-06 13:12:39 +0300

    Добавьте файлы проекта.

---
+++ zadanie_7/zadanie_7.cpp
@@ -0,0 +1,19 @@
+﻿// zadanie_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int num;
+        cout << "Введите число";
+    cin >> num;
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+            cout << i << endl;
+    }
+}
